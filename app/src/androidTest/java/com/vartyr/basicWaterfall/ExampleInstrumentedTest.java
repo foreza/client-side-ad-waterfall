76eda35160d4d2295b64fbb1a4b623073ef528aa
Jason Chiu <jason.chiu@inmobi.com>
2019-11-07 15:03:00 -0800

    bulk rename of sample project

--- app/src/androidTest/java/com/vartyr/basicWaterfall/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/vartyr/basicWaterfall/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.vartyr.inmobiconflictbindinglistenertest", appContext.getPackageName());
+        assertEquals("com.vartyr.waterfalltestapp", appContext.getPackageName());
     }
 }
